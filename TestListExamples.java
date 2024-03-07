25600e144b8ec47c32abfa31f173ee79817a0eaf
Thanh-Long Nguyen-Trong <thanhlong.nguyentrong@gmail.com>
2024-03-07 12:48:03 -0800

    lab 9

--- TestListExamples.java
+++ TestListExamples.java
@@ -1,5 +1,7 @@
 import static org.junit.Assert.*;
 import org.junit.*;
+
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -18,4 +20,19 @@ public class TestListExamples {
     List<String> expected = Arrays.asList("a", "a", "b", "c", "d");
     assertEquals(expected, merged);
   }
+
+  @Test
+  public void testFilter() {
+    List<String> strs = new ArrayList<>();
+    strs.add("a");
+    strs.add("b");
+    strs.add("apple");
+    List<String> filtered = ListExamples.filter(strs, s -> s.charAt(0) == 'a');
+    assertEquals(filtered, Arrays.asList("a", "apple"));
+
+    filtered = ListExamples.filter(strs, s -> s.charAt(0) == 'b');
+    assertEquals(filtered, Arrays.asList("b"));
+
+  }
+
 }
--- grade.sh
+++ grade.sh
@@ -1,5 +1,6 @@
 CPATH='.:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar'
 
+
 rm -rf student-submission
 rm -rf grading-area
 
@@ -7,6 +8,29 @@ mkdir grading-area
 
 git clone $1 student-submission
 echo 'Finished cloning'
+
+if [ -f "student-submission/ListExamples.java" ]; then 
+    echo "File found"
+else 
+    echo "File ListExamples.java not found!"
+    exit 1
+fi
+
+cp student-submission/ListExamples.java grading-area/
+cp TestListExamples.java grading-area/
+cp -r lib grading-area
+
+cd grading-area
+javac -cp $CPATH *.java
+
+if [ $? -ne 0 ]; then
+    echo "Compilation error"
+    exit 1
+fi
+
+echo "Program compiled successfully"
+
+java -cp .:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar org.junit.runner.JUnitCore TestListExamples
 
 
 # Draw a picture/take notes on the directory structure that's set up after
